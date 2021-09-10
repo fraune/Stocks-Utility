06bea17c4c90674a4d0261efa8729f7f64c47053
Brandon Fraune <brandon.fraune@directsupply.com>
2021-09-09 23:41:42 -0500

    add a bit of safety to the frequency field option

--- download/download.py
+++ download/download.py
@@ -1,16 +1,25 @@
 import json
+from enum import Enum
 
 import requests
 
 
+class Frequency(Enum):
+    y = "Yearly",
+    m = "Monthly",
+    d = "Daily"
+
+
 def download():
-    print("Hello. Input a stock ticker to download from Fidelity.")
-    x = input("  ticker: ")
-    get_data(x)
+    print("Hello. What would you like to download from Fidelity?")
+    ticker = input("  ticker: ")
+    frequency_character = input("  frequency (y/m/d): ")
+    frequency_string = Frequency[frequency_character].value[0]
+    get_data(ticker, frequency_string)
 
 
-def get_data(ticker):
-    url = f'https://fastquote.fidelity.com/service/marketdata/historical/chart/json?productid=research&symbols={ticker}&startDate=1970/01/01&endDate=2021/09/05&barWidth=Daily&extendedHours=N&quoteType=R&corpActions=Y&timestamp=start&uuid=6debdc8c-aea9-11e3-962f-9bc8b53daa77&callback=null'
+def get_data(ticker: str, frequency: str):
+    url = f'https://fastquote.fidelity.com/service/marketdata/historical/chart/json?productid=research&symbols={ticker}&startDate=1970/01/01&endDate=2021/09/05&barWidth={frequency}&extendedHours=N&quoteType=R&corpActions=Y&timestamp=start&uuid=6debdc8c-aea9-11e3-962f-9bc8b53daa77&callback=null'
     result = requests.get(url)
     json_response = convert_fidelity_text_response_to_json(result.text)
     response_dict = json.loads(json_response)
