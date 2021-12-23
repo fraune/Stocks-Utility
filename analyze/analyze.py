2df164b0207831c19042b77c61e0ffb317899afd
Brandon Fraune <brandon.fraune@directsupply.com>
2021-12-23 16:16:56 -0600

    add some incomplete test analysis code

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -1,8 +1,16 @@
 import datetime
 
+import pandas
+import seaborn
+
+FIDELITY_DATE_FORMAT = "%Y/%m/%d-%H:%M:%S"
+
 
 def analyze():
-    date_string = ''
-    # need to read from file
+    print('What downloaded ticker data would you like to analyze?')
+    ticker = input('  ticker: ')
+    data = pandas.read_json(f'data/{ticker}.json')
+    converted_timestamps = data["lt"].map(lambda s: datetime.datetime.strptime(s, FIDELITY_DATE_FORMAT))
+    data["lt"] = converted_timestamps
 
-    datetime.datetime.strptime(date_string, "%Y/%m/%d")
+    seaborn.displot(data, x="lt", y="cl")
